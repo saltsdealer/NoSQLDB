f1bc8df24649526127e87e7a920db90558c9b4c0
Tairan Ren <ren.tai@northeastern.edu>
2024-03-31 17:35:58 -0700

    almost there

---
+++ src/main/java/proj1/Main.java
@@ -0,0 +1,9 @@
+package proj1;
+ /*
+ *@Author : Tairan Ren
+ *@Date   : 2024/3/29 16:47
+ *@Title  : 
+ */
+    
+    public class Main {
+}
---
+++ src/test/java/proj1/lsmtree/application/ConsoleTest.java
@@ -0,0 +1,11 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+ /*
+ *@Author : Tairan Ren
+ *@Date   : 2024/3/26 16:45
+ *@Title  : 
+ */
+    
+    class ConsoleTest {
+  
+}
